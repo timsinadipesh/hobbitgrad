b2f6868586eef489496f47efe2a33546816abcc7
Dipesh Timsina <timsinadipesh77@gmail.com>
2025-08-11 02:43:43 +0545

    forward pass implemented

--- hobbitgrad/transform.py
+++ hobbitgrad/transform.py
@@ -1,42 +1,52 @@
-import random
-from math import exp
+import numpy as np
 
-random.seed(42)
+np.random.seed(42)
 
 def relu(x):
-    return max(x, 0)
- 
-def init_param(shape):
-    # initialize a parameter
-    return random.random()
+    return np.maximum(x, 0)
 
-def init_neuron(w_shape, b_shape):
-    # initialize a neuron 
-    return (init_param(w_shape), init_param(b_shape))
+class Layer:
+    def __init__(self, input_size, output_size):
+        self.W = np.random.rand(input_size, output_size)
+        self.b = np.zeros(output_size)
+        print("self.W.shape, self.b.shape:", self.W.shape, self.b.shape)
 
-def init_layer(num_neurons, num_prev_layer_neurons, num_next_layer_neurons):
-    # initialize a layer
-    layer = []
-    for i in range(num_neurons):
-        layer.append(init_neuron(num_prev_layer_neurons))
-    return layer
-
-def linear_forward(W, X, b):
-    Z = np.matmul(W, X) + b
-    # cache = (W, X, b) # implement this where this is called? because why create it here at all if just for this
-    return Z
+    def forward(self, x):
+        self.x = x
+        print("x.shape", x.shape)
+        print("self.W.shape, self.b.shape:", self.W.shape, self.b.shape)
+        print()
+        return x @ self.W + self.b
+    
+    def backward(self, dout):
+        ...
 
 class Model:
-    def __init__(self, data, layers, labels):
-        param_sizes = [[len(data[0]), layers[0]]]
-        for i in range(len(layers) - 1):
-            param_sizes.append([layers[i], layers[i+1]])
-        param_sizes.append([layers[-1], len(labels)])
-        print(param_sizes)
+    def __init__(self, layers):
+        layers.append(1)
+        self.layers = [Layer(layers[i], layers[i+1]) for i in range(len(layers) - 1)]
+        print("len(self.layers):", len(self.layers))
 
-data = [list(i for i in range(5)) for i in range(10)]
-labels = [i for i in range(10)]
-m = len(data)
-print(data)
-layers = [2 * m, 3 * m, 2 * m]
-model = Model(data=data, layers=layers, labels=labels)
+    def forward(self, data):
+        for layer in self.layers:
+            print("data.shape:",data.shape)
+            data = layer.forward(data)
+        print("data.shape:",data.shape)
+        print(data)       
+
+m = 10
+input_dim = 3
+
+X = np.random.rand(m, input_dim)
+print("X.shape:", X.shape)
+
+true_weights = np.array([3.0, -1.0, 6.5])
+true_bias = 3.0
+
+y = X @ true_weights + true_bias + np.random.randn(m) * 0.1
+y = y.reshape(-1, 1)
+print("y.shape:", y.shape)
+
+layers = [input_dim, 2 * input_dim, input_dim]
+model = Model(layers)
+model.forward(X)
