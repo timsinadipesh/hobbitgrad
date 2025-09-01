fd5459cb2ab3db3fb58d14fe1385927ac76135a1
Dipesh Timsina <timsinadipesh77@gmail.com>
2025-09-01 07:57:30 +0545

    mnist test v0

--- hobbitgrad/modules.py
+++ hobbitgrad/modules.py
@@ -13,23 +13,25 @@ class RELU:
         dX = d_out * (X > 0)
         return dX
     
+    def __call__(self, X):
+        return self.forward(X)
 
-class Neuron:
-    def __init__(self, input_size):
-        self.W = np.random.rand(input_size, 1).astype(np.float32)
-        self.b = np.zeros((1,), dtype=np.float32)
+
+class Linear:
+    def __init__(self, input_size, output_size):
+        self.W = np.random.rand(input_size, output_size).astype(np.float32)
+        self.b = np.zeros((output_size, ), dtype=np.float32)
         self.cache = None
         self.dW = None
         self.db = None
 
     def forward(self, X):
         self.cache = X
-        # print("    X.shape", X.shape)
-        neuron_out = X @ self.W + self.b
-        # print("    neuron_out.shape, neuron_out:", neuron_out.shape, neuron_out)
-        return neuron_out
+        # print("X.shape, self.W.shape, self.b.shape", X.shape, self.W.shape, self.b.shape)
+        return X @ self.W + self.b
     
     def backward(self, d_out):
+        # d_out shape: (batch_size, num_neurons)
         X = self.cache
         # Gradients for weights and bias
         self.dW = X.T @ d_out
@@ -38,31 +40,11 @@ class Neuron:
         dX = d_out @ self.W.T
         return dX
     
-    def __repr__(self):
-        return f"\n    Neuron(W.shape={self.W.shape}, b.shape={self.b.shape}, dtype={self.W.dtype})"
-
-
-class Dense:
-    def __init__(self, input_size, output_size):
-        self.neurons = [Neuron(input_size) for i in range(output_size)]
-
-    def forward(self, X):
-        self.cache = X
-        layer_outputs = [neuron.forward(X) for neuron in self.neurons]
-        return np.column_stack(layer_outputs) # output shape (n_samples, num_neurons)
-    
-    def backward(self, d_out):
-        # d_out shape: (batch_size, num_neurons)
-        grads = []
-        for i, neuron in enumerate(self.neurons):
-            dX_i = neuron.backward(d_out[:, [i]])
-            grads.append(dX_i)
-        # Sum gradients from all neurons
-        dX = np.sum(grads, axis=0)
-        return dX
+    def __call__(self, X):
+        return self.forward(X)
 
     def __repr__(self):
-        return f"\n  Dense(num_neurons={len(self.neurons)}, neurons={self.neurons})\n"
+        return f"\n  Linear(num_neurons={len(self.neurons)}, neurons={self.neurons})\n"
 
 
 class Model:
@@ -70,20 +52,25 @@ class Model:
         self.layers = layers
 
     def forward(self, X):
-        for i in range(len(self.layers)):
-            X = self.layers[i].forward(X)
+        for layer in self.layers:
+            X = layer(X)
         return X
     
     def backward(self, d_out):
-        for i in reversed(range(len(self.layers))):
-            d_out = self.layers[i].backward(d_out)
+        for layer in reversed(self.layers):
+            d_out = layer.backward(d_out)
         return d_out
+    
+    def __call__(self, X):
+        return self.forward(X)
     
     def __repr__(self):
         input_size = self.layers[0].neurons[0].W.shape[0]
         output_size = len(self.layers[-1].neurons)
         return f"Model(inputs={input_size}, num_layers={len(self.layers)}, outputs={output_size}, layers={self.layers})"
 
+...
+...
 
 class MSELoss:
     def __init__(self):
@@ -106,7 +93,12 @@ class SGD:
 
     def step(self):
         for layer in self.model.layers:
-            if isinstance(layer, Dense):
-                for neuron in layer.neurons:
-                    neuron.W -= self.lr * neuron.dW
-                    neuron.b -= self.lr * neuron.db
+            if isinstance(layer, Linear):
+                layer.W -= self.lr * layer.dW
+                layer.b -= self.lr * layer.db
+
+    def zero_grad(self):
+        for layer in self.model.layers:
+            if isinstance(layer, Linear):
+                layer.dW = None
+                layer.db = None
--- test_modules.py
+++ test_modules.py
@@ -2,10 +2,10 @@ from hobbitgrad.modules import *
 
 np.random.seed(42)
 
-m = 5
-features = 3
+m = 10
+input_dim = 3
 
-X = np.random.rand(m, features)
+X = np.random.rand(m, input_dim)
 
 true_weights = np.array([11.0, 13.0, -16.5])
 true_bias = 3.0
@@ -15,20 +15,24 @@ y = y.reshape(-1, 1)
 # print("y:\n", y)
 
 model = Model([
-    Dense(3, 6), RELU(), 
-    Dense(6, 3), RELU(), 
-    Dense(3, 1)
+    Linear(3, 6), RELU(), 
+    Linear(6, 3), RELU(), 
+    Linear(3, 1)
     ])
 # print(model)
 
 loss_fn = MSELoss()
 optimizer = SGD(model)
-epochs = 100
+epochs = 50
 
 for epoch in range(epochs):
+    optimizer.zero_grad()
+
     # forward
     y_pred = model.forward(X)
     loss = loss_fn.forward(y, y_pred)
+
+    print(f"Epoch {epoch}, Loss: {loss}")
 
     # backward
     d_out = loss_fn.backward()
@@ -37,10 +41,12 @@ for epoch in range(epochs):
     # update params
     optimizer.step()
 
-    print(f"Epoch {epoch}, Loss: {loss}")
 
 # final prediction
 y_pred = model.forward(X)
-print("\nFinal predictions:\n", y_pred)
-print("True y:", y)
-print("Loss:", loss_fn.forward(y, y_pred))
+# print("\nFinal predictions:\n", y_pred)
+# print("True y:\n", y)
+print("Final Loss:", loss_fn.forward(y, y_pred))
+
+# gotta test & update __repr__ functions. create a new thing.
+# always have tests for everything
